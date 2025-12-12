50407b40db60276cc7ee09888f7f82a5e3a6d63b
dhananjay sable <dhananjay.sable@thehirehq.com>
2025-12-12 12:29:13 +0530

    chage the button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,14 +14,17 @@ export default function Landing() {
             <div className="main-container">
                 <Carousel />
             </div>
-            <div className="text-center mt-10">
-                <button
-                    style={{ backgroundColor: 'red', }}
-                    className="px-6 py-2 bg-red-700 hover:bg-red-800 text-white !bg-red-700 !hover:bg-red-800"
-                    onClick={handleGetStarted}
-                >
-                    Get Started
-                </button>
+            <div className="text-center mt-10 ">
+                <div className="flex items-center h-screen text-center">
+                    <button
+                        style={{ width: '130px', height: '40px', border: 'none', textAlign:'center', borderRadius:'10px', marginLeft:"45%", hover:"#357ab8"}}
+                        className="bg-red-700 hover:bg-red-800 text-white text-lg font-semibold rounded"
+                        onClick={handleGetStarted}
+                    >
+                        Get Started
+                    </button>
+                </div>
+
             </div>
         </div>
     )
